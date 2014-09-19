ad66b18f2fa4e6efe38f3a1af494aa1f0654044a
Tomasz Dębek <tdebek@Mac-mini-Tomasz.local>
2014-09-19 15:35:31 +0200

    rails g migration AddAvatarColumnsToPosts avatar, zmieniamy migaracje zanim zrobimy rake db:migratei jak zmieniemy rake db:migrate , korzystamy z dokumentacji paperclip

---
+++ db/migrate/20140919133235_add_avatar_columns_to_posts.rb
@@ -0,0 +1,11 @@
+class AddAvatarColumnsToPosts < ActiveRecord::Migration
+  def self.up
+    add_attachment :posts, :avatar
+  end
+
+  def self.down
+    remove_attachment :posts, :avatar
+  end
+
+
+end
